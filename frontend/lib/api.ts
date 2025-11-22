b3696721b289999a8df6877c735162b2d55dad42
Ahmad Hassan <ahmad@pfida.com>
2025-11-22 20:23:36 +0500

    api fix

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  // baseURL: 'https://api.1800eichlers.midpear.com/api',
+  // baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://api.1800eichlers.midpear.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
